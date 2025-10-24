a373391d95660720a6420f8e19a409a99dbe4017
jumontenegrol <jumontenegrol@unal.edu.co>
2025-10-24 10:23:39 -0500

    Cambion en el estilo, añade Style.css

--- gui2.c
+++ gui2.c
@@ -288,7 +288,7 @@ int main(int argc, char *argv[]) {
     gtk_init(&argc, &argv);
 
     GtkCssProvider *provider = gtk_css_provider_new();
-    gtk_css_provider_load_from_path(provider, "style.css", NULL);
+    gtk_css_provider_load_from_path(provider, "Style.css", NULL);
     gtk_style_context_add_provider_for_screen(
         gdk_screen_get_default(),
         GTK_STYLE_PROVIDER(provider),
